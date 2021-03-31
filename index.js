889789c644a416044d0b5f13e0af0bd99a1063be
Rao-Jibran <raojibrankhan@gmail.com>
2021-03-31 23:02:32 +0500

    pushhhh

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function signUP(){
+    var emailId=document.getElementById("email").value 
+    var password=document.getElementById("pass").value
+
+    localStorage.setItem("userId",emailId)
+    localStorage.setItem("userPass",password)
+
+    window.location.href="login.html"
+   
+}
+
+function back(){
+    window.location.href="signup.html"
+}
+
+function signIn(){
+    var userEmail=document.getElementById("userEmail").value
+    var userPass=document.getElementById("userPass").value
+    console.log(userEmail)
+    console.log(userPass)
+    var id= localStorage.getItem("userId")
+    var pass= localStorage.getItem("userPass")
+    if(userEmail===id && userPass===pass){
+        window.location.href="index.html"
+
+    }
+    else{
+        alert("invalid")
+    }
+    
+}
